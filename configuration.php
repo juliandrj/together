6be196aaa87eaade227a4ec1fe27cae8bc7bd983
juliandrj <juliandrj@gmail.com>
2015-02-22 17:53:55 -0500

    El proyecto eclipse y un pequeño arreglo en la configuración.

--- configuration.php
+++ configuration.php
@@ -54,8 +54,8 @@ class JConfig {
 	public $sef_suffix = '0';
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
-	public $log_path = '/Users/juliandrj/Sites/logs';
-	public $tmp_path = '/Users/juliandrj/Sites/tmp';
+	public $log_path = '/home/juliandrj/public_html/logs';
+	public $tmp_path = '/home/juliandrj/public_html/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 	public $memcache_persist = '1';
